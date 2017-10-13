eb75c2984e4ea8393e3eaff448395cb969cfaa4c
Allan Dollé <dolle.allan@gmail.com>
2017-10-13 21:22:37 +0200

    Some wording change and css optimization

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="entete flex-vertical">
     <img src="//www.anapath.fr/wp-content/uploads/2017/06/logo-300px.png" alt="Logo Laboratoire Anapathologie Amiens" />
-    <h1>Paiement en ligne de votre examen d'anapathologie</h1>
+    <h1>Payez en ligne votre examen d'anapathologie</h1>
   </header>
 </template>
 <script src='./header.js'></script>
--- src/components/info/Info.vue
+++ src/components/info/Info.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="info" id='res'>
-    <p>Pour procéder au paiement d'un examen, veuillez entrer votre <strong>Nom de Famille</strong> ainsi que le <strong>Numéro de Facture</strong> figurant sur votre reçu.</p>
+    <p>Pour procéder au paiement d'un examen, veuillez entrer votre <strong>Nom de Famille</strong> ainsi que le <strong>Numéro de Facture</strong> figurant sur votre facture.</p>
   </section>
 </template>
 <script src='./info.js'></script>
