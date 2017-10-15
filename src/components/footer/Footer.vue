d1ed62d956cb8e40c93b147086fc0b5f6c17c3fd
Allan Dollé <dolle.allan@gmail.com>
2017-10-15 04:44:46 +0200

    Add meta, favicon, and some design things

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -1,5 +1,6 @@
 <template>
   <footer class="flex-horizontal piedpage">
+    <p class="flex-horizontal"><span></span><span></span><span></span><span></span></p>
     <p>Copyright © 2017 <a href="//anapath.fr" title="retourner sur le site internet de votre laboratoire d'anapathologie" rel="author">anapath.fr</a> | Crédits <a href="//comuneidee.fr" title="Agence de communication a Amiens">comuneidee.fr</a></p>
   </footer>
 </template>
