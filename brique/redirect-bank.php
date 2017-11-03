633ef22e4f5b0e4ff5c9a8042cd10bad4e8865dc
Allan Dollé <dolle.allan@gmail.com>
2017-11-03 11:06:52 +0100

    HMAC delete ... lol

--- brique/redirect-bank.php
+++ brique/redirect-bank.php
@@ -136,8 +136,7 @@ a:hover {
 		$pbx_repondre_a = $url_server.$dir_paiement.'retour.php';
 		$pbx_retour = 'MONTANT:M;REF:R;AUTO:A;CB:J;TRANSAC:S;ERROR:E;SIGN:K';
 
-		$key_prod = '00a13882045cc2f316e021ba1970b4159909339d28aca1be00f70366c63e76c9681a7d28d7c09e767406b8dbfdabb03abe92c6474237a76b6076ae7bc63eacf9';
-		$key_dev = '05fc02827bc38bcbcf4c07e32d91ddb98b412fdd53ef1a0e759484bbc57c7acbc27ce0836fd6c6a3140ab93a1f4646e1de2ff35cbe2798f971a19bd4bb201662';
+		include 'utils/hmac.php';
 
 		// Choix de la clé HMAC en fonction de l'environnement
 		$key_hmac = $env_dev ? $key_dev : $key_prod;
