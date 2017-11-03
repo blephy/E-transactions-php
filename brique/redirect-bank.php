c221172784d37d833377b8ad2eb34321a6a68f09
Allan Dollé <dolle.allan@gmail.com>
2017-11-03 13:01:19 +0100

    Check Amount, convert it to euro centimes, and fail gracefully if amount is under 1€

--- brique/redirect-bank.php
+++ brique/redirect-bank.php
@@ -109,6 +109,25 @@ a:hover {
 	// Production ou preprod (dev)
 	$env_dev = true;
 
+	// Function de traitement de la chaine montant
+	function checkAmount( $montant_query ) {
+		$temp = str_replace(",", ".", $montant_query); // replace , par .
+		// echo $temp.'<br>';
+		$temp = round($temp, 2, PHP_ROUND_HALF_UP);
+		// echo $temp.'<br>';
+		$temp = floatval( $temp ); // convert to float
+		// echo $temp.'<br>';
+		$temp = $temp * 100;
+		// echo 'temp: '.$temp.'<br>';
+		$temp = str_replace(".", "", $temp);
+		// echo 'final temp: '.$temp.'<br>';
+		if ( $temp > 99 ) {
+			return $temp;
+		} else {
+			return false;
+		}
+	}
+
 	// Variables propre au client E-transactions
 	$pbx_site = '1542364';
 	$pbx_rang = '01';
@@ -118,100 +137,113 @@ a:hover {
 	if ( isset($_GET['ref']) && isset($_GET['porteur']) && isset($_GET['montant']) ) {
 		$pbx_cmd = $_GET['ref'];
 		$pbx_porteur = $_GET['porteur'];
-		$pbx_total = $_GET['montant'];
+		$pbx_total = checkAmount($_GET['montant']);
 
-		// Suppression des points ou virgules dans le montant
-		$pbx_total = str_replace(",", "", $pbx_total);
-		$pbx_total = str_replace(".", "", $pbx_total);
+		if ($pbx_total) {
+			// URL de retour serveur
+			$url_server = 'http://www.anapath.fr/';
+			$dir_paiement = 'test/brique/';
 
-		// URL de retour serveur
-		$url_server = 'http://www.anapath.fr/';
-		$dir_paiement = 'test/brique/';
+			// Détails URL de redirection après paiement
+			$pbx_effectue = $url_server.$dir_paiement.'accepte.php';
+			$pbx_annule = $url_server.$dir_paiement.'annule.php';
+			$pbx_refuse = $url_server.$dir_paiement.'refuse.php';
+			$pbx_attente = $url_server.$dir_paiement.'attente.php';
+			$pbx_repondre_a = $url_server.$dir_paiement.'retour.php';
+			$pbx_retour = 'MONTANT:M;REF:R;AUTO:A;CB:J;TRANSAC:S;ERROR:E;SIGN:K';
 
-		// Détails URL de redirection après paiement
-		$pbx_effectue = $url_server.$dir_paiement.'accepte.php';
-		$pbx_annule = $url_server.$dir_paiement.'annule.php';
-		$pbx_refuse = $url_server.$dir_paiement.'refuse.php';
-		$pbx_attente = $url_server.$dir_paiement.'attente.php';
-		$pbx_repondre_a = $url_server.$dir_paiement.'retour.php';
-		$pbx_retour = 'MONTANT:M;REF:R;AUTO:A;CB:J;TRANSAC:S;ERROR:E;SIGN:K';
+			// Include HMAC keys
+			include 'utils/hmac.php';
 
-		include 'utils/hmac.php';
+			// Choix de la clé HMAC en fonction de l'environnement
+			$key_hmac = $env_dev ? $key_dev : $key_prod;
 
-		// Choix de la clé HMAC en fonction de l'environnement
-		$key_hmac = $env_dev ? $key_dev : $key_prod;
+			$server_preprod = 'preprod-tpeweb.e-transactions.fr';
+			$server_prod = 'tpeweb.e-transactions.fr';
 
-		$server_preprod = 'preprod-tpeweb.e-transactions.fr';
-		$server_prod = 'tpeweb.e-transactions.fr';
+			// Choix du serveur e-transactions en fonction de l'environnement
+			$env_server = $env_dev ? $server_preprod : $server_prod;
 
-		// Choix du serveur e-transactions en fonction de l'environnement
-		$env_server = $env_dev ? $server_preprod : $server_prod;
-
-		$server_etransactions = 'https://'.$env_server.'/cgi/MYchoix_pagepaiement.cgi';
+			$server_etransactions = 'https://'.$env_server.'/cgi/MYchoix_pagepaiement.cgi';
 
 
 
-		// --------------- TRAITEMENT DES VARIABLES ---------------
+			// --------------- TRAITEMENT DES VARIABLES ---------------
 
-		$dateTime = date("c");
+			$dateTime = date("c");
 
-		$msg = "PBX_SITE=".$pbx_site.
-		"&PBX_RANG=".$pbx_rang.
-		"&PBX_IDENTIFIANT=".$pbx_identifiant.
-		"&PBX_TOTAL=".$pbx_total.
-		"&PBX_DEVISE=978".
-		"&PBX_CMD=".$pbx_cmd.
-		"&PBX_PORTEUR=".$pbx_porteur.
-		"&PBX_REPONDRE_A=".$pbx_repondre_a.
-		"&PBX_RETOUR=".$pbx_retour.
-		"&PBX_EFFECTUE=".$pbx_effectue.
-		"&PBX_ANNULE=".$pbx_annule.
-		"&PBX_REFUSE=".$pbx_refuse.
-		"&PBX_HASH=SHA512".
-		"&PBX_TIME=".$dateTime;
-		$binKey = pack("H*", $key_hmac);
-		$hmac = strtoupper(hash_hmac('sha512', $msg, $binKey));
-		?>
+			$msg = "PBX_SITE=".$pbx_site.
+			"&PBX_RANG=".$pbx_rang.
+			"&PBX_IDENTIFIANT=".$pbx_identifiant.
+			"&PBX_TOTAL=".$pbx_total.
+			"&PBX_DEVISE=978".
+			"&PBX_CMD=".$pbx_cmd.
+			"&PBX_PORTEUR=".$pbx_porteur.
+			"&PBX_REPONDRE_A=".$pbx_repondre_a.
+			"&PBX_RETOUR=".$pbx_retour.
+			"&PBX_EFFECTUE=".$pbx_effectue.
+			"&PBX_ANNULE=".$pbx_annule.
+			"&PBX_REFUSE=".$pbx_refuse.
+			"&PBX_HASH=SHA512".
+			"&PBX_TIME=".$dateTime;
+			$binKey = pack("H*", $key_hmac);
+			$hmac = strtoupper(hash_hmac('sha512', $msg, $binKey));
+			?>
+			<div class="entete">
+				<h1>Redirection en cours ...</h1>
+			</div>
+			<span class="loading"></span>
+			<div class="info">
+				<p>Cliquez sur <strong>Réessayer</strong> en cas de non redirection automatique après 2 secondes:</p>
+			</div>
+			<form id="form" style="text-align: center; margin: 20px auto;" method="POST" action="<?php echo $server_etransactions; ?>">
+				<input type="hidden" name="PBX_SITE" value="<?php echo $pbx_site; ?>">
+				<input type="hidden" name="PBX_RANG" value="<?php echo $pbx_rang; ?>">
+				<input type="hidden" name="PBX_IDENTIFIANT" value="<?php echo $pbx_identifiant; ?>">
+				<input type="hidden" name="PBX_TOTAL" value="<?php echo $pbx_total; ?>">
+				<input type="hidden" name="PBX_DEVISE" value="978">
+				<input type="hidden" name="PBX_CMD" value="<?php echo $pbx_cmd; ?>">
+				<input type="hidden" name="PBX_PORTEUR" value="<?php echo $pbx_porteur; ?>">
+				<input type="hidden" name="PBX_REPONDRE_A" value="<?php echo $pbx_repondre_a; ?>">
+				<input type="hidden" name="PBX_RETOUR" value="<?php echo $pbx_retour; ?>">
+				<input type="hidden" name="PBX_EFFECTUE" value="<?php echo $pbx_effectue; ?>">
+				<input type="hidden" name="PBX_ANNULE" value="<?php echo $pbx_annule; ?>">
+				<input type="hidden" name="PBX_REFUSE" value="<?php echo $pbx_refuse; ?>">
+				<input type="hidden" name="PBX_HASH" value="SHA512">
+				<input type="hidden" name="PBX_TIME" value="<?php echo $dateTime; ?>">
+				<input type="hidden" name="PBX_HMAC" value="<?php echo $hmac; ?>">
+				<input type="submit" value="Réessayer">
+			</form>
+		</body>
+		<script>
+		// window.onload=function(){
+		// 	var auto = setTimeout(function(){ autoRefresh(); }, 100);
+		//
+		// 	function submitform(){
+		// 		document.forms[0].submit();
+		// 	}
+		//
+		// 	function autoRefresh(){
+		// 		clearTimeout(auto);
+		// 		auto = setTimeout(function(){ submitform(); autoRefresh(); }, 2000);
+		// 	}
+		// }
+		</script>
+	<?php } else { ?>
 		<div class="entete">
-			<h1>Redirection en cours ...</h1>
+			<h1>Erreur du montant</h1>
 		</div>
-		<span class="loading"></span>
 		<div class="info">
-			<p>Cliquez sur <strong>Réessayer</strong> en cas de non redirection automatique après 2 secondes:</p>
+			<p class="alert">Le montant est inférieur à 1€. Transaction impossible</p>
+			<p class="alert">Merci de contacter votre Centre de Pathologie sur <a href="mailto:contact@anapath.fr" title="Envoyer un e-mail au Centre de Pathologie des Hauts de France">contact@anapath.fr</a></p>
+			<button onclick="goBack()">Retour</button>
 		</div>
-		<form id="form" style="text-align: center; margin: 20px auto;" method="POST" action="<?php echo $server_etransactions; ?>">
-			<input type="hidden" name="PBX_SITE" value="<?php echo $pbx_site; ?>">
-			<input type="hidden" name="PBX_RANG" value="<?php echo $pbx_rang; ?>">
-			<input type="hidden" name="PBX_IDENTIFIANT" value="<?php echo $pbx_identifiant; ?>">
-			<input type="hidden" name="PBX_TOTAL" value="<?php echo $pbx_total; ?>">
-			<input type="hidden" name="PBX_DEVISE" value="978">
-			<input type="hidden" name="PBX_CMD" value="<?php echo $pbx_cmd; ?>">
-			<input type="hidden" name="PBX_PORTEUR" value="<?php echo $pbx_porteur; ?>">
-			<input type="hidden" name="PBX_REPONDRE_A" value="<?php echo $pbx_repondre_a; ?>">
-			<input type="hidden" name="PBX_RETOUR" value="<?php echo $pbx_retour; ?>">
-			<input type="hidden" name="PBX_EFFECTUE" value="<?php echo $pbx_effectue; ?>">
-			<input type="hidden" name="PBX_ANNULE" value="<?php echo $pbx_annule; ?>">
-			<input type="hidden" name="PBX_REFUSE" value="<?php echo $pbx_refuse; ?>">
-			<input type="hidden" name="PBX_HASH" value="SHA512">
-			<input type="hidden" name="PBX_TIME" value="<?php echo $dateTime; ?>">
-			<input type="hidden" name="PBX_HMAC" value="<?php echo $hmac; ?>">
-			<input type="submit" value="Réessayer">
-		</form>
-	</body>
-	<script>
-	window.onload=function(){
-		var auto = setTimeout(function(){ autoRefresh(); }, 100);
-
-		function submitform(){
-			document.forms[0].submit();
+		<script>
+		function goBack() {
+			window.history.back();
 		}
-
-		function autoRefresh(){
-			clearTimeout(auto);
-			auto = setTimeout(function(){ submitform(); autoRefresh(); }, 2000);
-		}
-	}
-	</script>
+		</script>
+	<?php }	?>
 <?php } else { ?>
 	<div class="entete">
 		<h1>Problème du formulaire</h1>
