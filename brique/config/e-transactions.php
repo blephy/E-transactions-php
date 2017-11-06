b04e4b768eb69de27809d42da74547cc6b4f4525
Allan Dollé <dolle.allan@gmail.com>
2017-11-06 14:28:03 +0100

    Fix issue 403 on retour.php (IP Verifone ???) and fix issue server url incorrect

--- brique/config/e-transactions.php
+++ brique/config/e-transactions.php
@@ -5,6 +5,6 @@ $server_preprod = 'preprod-tpeweb.e-transactions.fr';
 $server_prod = 'tpeweb.e-transactions.fr';
 
 // Changer par votre choix, cf doc
-$server_file_desktop = '/php';
+$server_file_desktop = '/php/';
 $server_file_mobile = '/cgi/ChoixPaiementMobile.cgi';
 ?>
