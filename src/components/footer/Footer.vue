aa5391ee5a2855a6918217f39dd000a11d884544
Allan Dollé <dolle.allan@gmail.com>
2017-11-15 13:06:18 +0100

    Footer title <a> change

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="flex-horizontal piedpage">
     <p class="flex-horizontal"><span></span><span></span><span></span><span></span></p>
-    <p>Copyright © 2017 <a href="//anapath.fr" title="retourner sur le site internet de votre laboratoire d'anapathologie" rel="author">anapath.fr</a> | Crédits <a href="//comuneidee.fr" title="Agence de communication à Amiens">comuneidee.fr</a></p>
+    <p>Copyright © 2017 <a href="//anapath.fr" title="Retourner sur le site internet du Centre de Pathologie des Hauts-de-France" rel="author">anapath.fr</a> | Crédits <a href="//comuneidee.fr" title="Agence de communication à Amiens">comuneidee.fr</a></p>
   </footer>
 </template>
 <script src='./footer.js'></script>
