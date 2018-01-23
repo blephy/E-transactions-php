7ea5c17f90cab49baf198291892d87c816c4edad
Allan Dollé <dolle.allan@gmail.com>
2018-01-23 10:20:24 +0100

    Wording

--- src/components/browser-alert/BrowserAlert.vue
+++ src/components/browser-alert/BrowserAlert.vue
@@ -2,7 +2,7 @@
   <section>
   <HeaderAnapath/>
   <section class="info">
-    <p>Vous utilisez un navigateur internet (Internet Explorer) qui <strong>n'est plus mise à jour</strong> par son propriétaire et est <strong>obsolète</strong>.<br>Pour des raisons de sécurité, notre service vous est indisponible.<br><br>Pour pouvoir Régler par internet votre examen d'anatomie pathologique, <strong>veuillez utiliser un autre navigateur gratuit, récent et à jour</strong> :
+    <p>Vous utilisez un navigateur internet (Internet Explorer) qui <strong>n'est plus mis à jour</strong>.<br>Pour des raisons de sécurité, notre service de paiement en ligne vous est inaccessible.<br><br>Pour pouvoir Régler par internet votre examen, <strong>vous devez utiliser un autre navigateur gratuit, récent et à jour</strong> :
     <a href="https://www.mozilla.org/fr/firefox/" title="Installez Firefox">Firefox</a>, <a href="https://www.google.fr/chrome/browser/desktop/index.html" title="Installez Chrome">Chrome</a>, <a href="https://support.apple.com/fr_FR/downloads/safari" title="Installez Safari">Safari</a>, <a href="https://www.microsoft.com/fr-fr/windows/microsoft-edge" title="Installez Edge">Edge</a>.<br> Puis revenez sur cette page en utilisant l'un de ceux proposés.</p>
   </section>
 </section>
